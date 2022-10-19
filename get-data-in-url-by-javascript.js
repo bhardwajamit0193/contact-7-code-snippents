266ee6f242131afb5bb9ec882492103fecbff537
Amit Bhardwaj <73641309+bhardwajamit0193@users.noreply.github.com>
2022-10-19 14:32:05 +0530

    Update get-data-in-url-by-javascript.js

--- get-data-in-url-by-javascript.js
+++ get-data-in-url-by-javascript.js
@@ -1,5 +1,9 @@
 // Example url have two parameter Name & Email Id  https/example.com/thank-you/?yourname=amit&youremail=+youremail;
 
+// if not error come in  this url https/example.com/thank-you/?yourname=amit&youremail=+youremail 
+// then use this  https/example.com/thank-you/?yourname-amit&youremail-+youremail;
+// then  add this query_string =query_string.replaceAll('-', '='); below  var query_string=window.location.search;
+
 <script>
   var query_string=window.location.search;
 var urlParmArr=new URLSearchParams(query_string);
